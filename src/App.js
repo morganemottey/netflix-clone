b01abb3187b3a4aa37be50855c253729e1b0f89f
Morgane Mottey <bob@example.com>
2021-01-04 14:40:37 +0100

    add api with axios

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './routes/Home'
-import myImage from './assets/images/Fast_large.jpg'
-import mySecondImage from './assets/images/Fast_small.jpg'
-
 import './App.css'
+import myImage from './assets/images/Fast_large.jpg'
+import axios from 'axios'
+import { API_KEY , API_URL , IMAGE_BASE_URL , BACKDROP_SIZE, POSTER_SIZE } from './config'
 
 export default class App extends Component {
   state = {
@@ -44,11 +44,33 @@ export default class App extends Component {
     totalPages: 0,
     searchText: ""
   }
+  async componentDidMount () {
+    try {
+        const { data : { results, page, total_pages }} = await this.loadMovies();
+        console.log('res', results);
+        this.setState({
+          movies: results,
+          loading: false,
+          activePage: page,
+          totalPages: total_pages,
+          image: `${IMAGE_BASE_URL}/${BACKDROP_SIZE}/${results[0].backdrop_path}`,
+          mTitle: results[0].title,
+          mDesc: results[0].overview
+        })
+    } catch(e) {
+      console.log('e', e);
+    }
+  }
   handleSearch = value => {
     console.log('handleSearch', value)
   }
   loadMore () {
     console.log('loadMore')
+  }
+  loadMovies () {
+    const page = this.state.activePage + 1;
+    const url = `${API_URL}/movie/popular?api_key=${API_KEY}&page=${page}&language=fr`;
+    return axios.get(url)
   }
   render() {
     return (
--- src/components/PosterList.js
+++ src/components/PosterList.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
-import Poster from './Poster'
-import '../css/PosterList.css'
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import Poster from './Poster';
+import '../css/PosterList.css';
 
 let wish;
 
 class PosterList extends Component {
     renderPoster = () => {
         return this.props.movies.map(movie => {
-            const imgSrc = `${movie.poster_path}`;
+            const imgSrc = `${IMAGE_BASE_URL}/${POSTER_SIZE}/${movie.poster_path}`;
             wish = false;
+            if(this.props.localMovies) {
+                this.props.localMovies.forEach(localMovie => {
+                    if(movie.id === localMovie.id) {
+                        wish = true
+                    }
+                })
+            }
             return (
                 <Poster 
                     key={movie.id}
@@ -28,11 +36,11 @@ class PosterList extends Component {
             <div className="posterList">
                 <h3 className="posterList--title">NOUVEAUX FILMS</h3>
                 <div className="posterList--grid">
-                    {this.renderPoster}
+                    {this.renderPoster()}
                 </div>
             </div>
         )
     }
 }
 
-export default PosterList
+export default PosterList 
