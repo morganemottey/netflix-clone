5e8d92c8dd21a55434a55def0289084e4bcbd88f
Morgane Mottey <bob@example.com>
2021-01-05 11:40:55 +0100

    add 2 function into utils to convert money and hour

--- src/components/HeaderDetails.js
+++ src/components/HeaderDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Container from './Container'
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import { calcTime , convertMoney } from '../utils/helpers'
 import Stars from './Stars'
 
 import '../css/HeaderDetails.css'
@@ -18,9 +19,9 @@ export default class HeaderDetails extends Component {
                     <h3 className="headerDetails--container__title">{this.props.mTitle}</h3>
                     <p className="headerDetails--container__desc">{this.props.mDesc}</p>
                     <div className="headerDetails--info">
-                        <Container iconName="hourglass-half" content={this.props.runtime} />
+                        <Container iconName="hourglass-half" content={calcTime(this.props.runtime)} />
                         <Stars fakeArray1={["1","2","3"]} fakeArray2={["1","2"]} />
-                        <Container iconName="money" content={this.props.revenue} />
+                        <Container iconName="money" content={convertMoney(this.props.revenue)} />
                     </div>
                 </div>
             </div>
