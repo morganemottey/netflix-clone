3c14b8958281c8a43252ef4df716862d1e72c33f
Morgane Mottey <bob@example.com>
2021-01-05 12:10:47 +0100

    calculate vote  with function

--- src/components/HeaderDetails.js
+++ src/components/HeaderDetails.js
@@ -7,7 +7,22 @@ import Stars from './Stars'
 import '../css/HeaderDetails.css'
 
 export default class HeaderDetails extends Component {
+    calcVote = () => {
+        this.fakeArray1 = [];
+        this.fakeArray2 = [];
+        const vote = Math.round(this.props.vote/2);
+        const reste = 5 - vote;
+        for(let i = 0; i < vote; i++) {
+            this.fakeArray1.push("1");
+        }
+        if(reste !== 0) {
+            for(let i = 0; i < reste; i++) {
+                this.fakeArray2.push('1');
+            }
+        }
+    }
     render() {
+        this.calcVote();
         const imgSrc = `${IMAGE_BASE_URL}/${POSTER_SIZE}/${this.props.imgSrc}`;
         return (
             <div className="headerDetails">
@@ -20,7 +35,7 @@ export default class HeaderDetails extends Component {
                     <p className="headerDetails--container__desc">{this.props.mDesc}</p>
                     <div className="headerDetails--info">
                         <Container iconName="hourglass-half" content={calcTime(this.props.runtime)} />
-                        <Stars fakeArray1={["1","2","3"]} fakeArray2={["1","2"]} />
+                        <Stars fakeArray1={this.fakeArray1} fakeArray2={this.fakeArray2} />
                         <Container iconName="money" content={convertMoney(this.props.revenue)} />
                     </div>
                 </div>
